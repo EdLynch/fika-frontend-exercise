78871c7456a24470b69298794de1ffdb9b05df13
Ed <edlynchdev@gmail.com>
2022-04-27 09:11:26 +0100

    Add genres to movie modal

--- src/components/ActiveMovieModal.tsx
+++ src/components/ActiveMovieModal.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Movie } from '../types';
+import { useQuery } from 'react-query';
+import { getGenres } from '../api/getGenres';
 
 type Props = {
   activeMovie?: Movie;
@@ -7,13 +9,22 @@ type Props = {
 };
 
 export default function ActiveMovieModal({ activeMovie, close }: Props) {
+  const { data } = useQuery('genres', getGenres);
+
+  let genres = data?.genres || [];
   if (activeMovie === null || activeMovie === undefined) return null;
-  const { title, poster_path, vote_average, overview } = activeMovie;
+  const { title, poster_path, vote_average, overview, genre_ids } = activeMovie;
+
+  // Avoid empty element issues if a genere gets removed or can't find id
+  const movieGenres = genre_ids
+    .map((id) => genres.find((genre) => genre.id === id)?.name)
+    .filter((genre) => genre !== undefined);
 
   return (
     <div className='flex items-center justify-center fixed top-0 left-0 w-[100vw] h-[100vh] z-50'>
       <div className='relative flex flex-col gap-4 items-center z-30 bg-white p-8 mx-12 rounded-xl max-w-xl max-h-[90%] overflow-scroll'>
         <h1 className='text-xl'>{title}</h1>
+        {genres && <div>{movieGenres.join(', ')}</div>}
         <img
           src={`${process.env.REACT_APP_BASE_IMG_PATH}${poster_path}`}
           className='top-0 left-0 w-52 z-10'
